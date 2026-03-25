fd6074c9ce9bb890071ec21e89ccc5d58200ab23
Levi <levi.zopff@gmail.com>
2026-03-25 01:58:19 -0700

    Tracks mouse position x/y

--- Portfolio-WebApp/src/pages/Contact.jsx
+++ Portfolio-WebApp/src/pages/Contact.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+
+function MouseLocation() {
+
+    const [mouseLocation, setMouseLocation] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+
+        const handleMouseMove = (event) => {
+            setMouseLocation({
+                x: event.clientX,
+                y: event.clientY,
+            })
+        }
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        }
+
+    }, [])
+    return (
+        <span>
+            X: {mouseLocation.x} Y: {mouseLocation.y}
+        </span>
+    )
+}
+
 function Contact() {
     return (
-        <div>
-            <h1 className="contact-page-header">Contact Page</h1>
-        </div>
+        <body>
+            <div>
+                <h1 className="contact-page-header" {...console.log("Hello")}>Contact Page</h1>
+            </div>
+            <div>
+                <h1>{<MouseLocation />}</h1>
+            </div>
+        </body>
     )
 }
 
